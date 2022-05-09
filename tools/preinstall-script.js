b2a32bd0df1f3e72df2b57d68d9dd57ef35a8feb
Narongrit Unwerawattana <narongrit.kie@gmail.com>
2022-05-09 06:43:49 +0200

    hooks scripts

--- tools/preinstall-script.js
+++ tools/preinstall-script.js
@@ -1,8 +1,8 @@
 /**
  * Do NOT allow using `npm` as package manager.
  */
-if (process.env.npm_execpath.indexOf("jpm") === -1) {
-  console.error("You must use JPM to install dependencies:");
-  console.error("  $ jpm install");
-  process.exitCode = 1;
-}
+// if (process.env.npm_execpath.indexOf("jpm") === -1) {
+//   console.error("You must use JPM to install dependencies:");
+//   console.error("  $ jpm install");
+//   process.exitCode = 1;
+// }
